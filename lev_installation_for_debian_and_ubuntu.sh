64801abb6251e6b3b982a4410f837bbb3e8d8578
Haotian.Wang <whtcjdtc2007@163.com>
2022-04-25 01:34:52 +0800

    v 0.25
    Update:
    1. Add support for Debian 7/8;

--- lev_installation_for_debian_and_ubuntu.sh
+++ lev_installation_for_debian_and_ubuntu.sh
@@ -11,7 +11,7 @@ elif [ "$os_type" = "Ubuntu" ]; then
     ubuntu_version=$(lsb_release -r -s)
 fi
 
-if [[ "$debian_version" =~ 9\.* ]]; then
+if [[ "$debian_version" =~ [789]\.* ]]; then
     apt update && apt upgrade -y
     # install openssl-1.1.1n
 	apt install build-essential checkinstall zlib1g-dev wget -y
@@ -41,7 +41,7 @@ if [[ "$debian_version" =~ 9\.* ]]; then
     curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3.10 -
     echo "export PATH=/root/.local/bin:\$PATH" >> ~/.bashrc
     source ~/.bashrc
-elif [ "$debian_version" = "10" ] || [ "$debian_version" = "11" ]; then
+elif [[ "$debian_version" =~ 10\.* ]] || [[ "$debian_version" =~ 11\.* ]]; then
     apt update && apt upgrade -y
     # install python-3.10.4
     apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
@@ -99,7 +99,7 @@ elif [ "$ubuntu_version" = "14.04" ] || [ "$ubuntu_version" = "16.04" ]; then
 	curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3.10 -
 	echo "export PATH=/root/.local/bin:\$PATH" >> ~/.bashrc
 	source ~/.bashrc
-elif [ "$ubuntu_version" = "20.04" ] || [ "$ubuntu_version" = "18.04" ]; then
+elif [ "$ubuntu_version" = "18.04" ] || [ "$ubuntu_version" = "20.04" ]; then
 	# install python3.10.4
 	sudo apt update && sudo apt upgrade -y
 	sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
